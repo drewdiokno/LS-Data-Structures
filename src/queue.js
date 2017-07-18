6f06af11c6d7308111afedd9ab91149fdfd8ea3e
kiyoshi626 <travis.nakano@gmail.com>
2017-07-17 22:27:03 -0500

    updated queue and stack files

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,16 @@
 class Queue {
-
+  constructor() {
+    this.storage = [];
+  }
+  enqueue(value) {
+    return this.storage.push(value);
+  }
+  dequeue(value) {
+    return this.storage.shift(value);
+  }
+  get size() {
+    return this.storage.length;
+  }
 }
 
 module.exports = Queue;
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,16 @@
 class Stack {
-
+  constructor() {
+    this.storage = [];
+  }
+  add(value) {
+    return this.storage.push(value);
+  }
+  remove(value) {
+    return this.storage.pop(value);
+  }
+  get size() {
+    return this.storage.length;
+  }
 }
 
 module.exports = Stack;
